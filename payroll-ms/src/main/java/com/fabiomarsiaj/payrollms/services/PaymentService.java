a31c62d2691d4ae7c7ab6beced3f4d8373efe830
Fabio <fabiomarsiaj@gmail.com>
2020-11-24 16:40:00 -0300

    implemented feign client to comunicate with workerms

--- payroll-ms/src/main/java/com/fabiomarsiaj/payrollms/services/PaymentService.java
+++ payroll-ms/src/main/java/com/fabiomarsiaj/payrollms/services/PaymentService.java
@@ -2,27 +2,18 @@ package com.fabiomarsiaj.payrollms.services;
 
 import com.fabiomarsiaj.payrollms.domains.Payment;
 import com.fabiomarsiaj.payrollms.domains.Worker;
-import java.util.HashMap;
-import java.util.Map;
+import com.fabiomarsiaj.payrollms.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriParams = new HashMap<>();
-        uriParams.put("id", String.valueOf(workerId));
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriParams);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
