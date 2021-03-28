9799e2c89e13db3c4d5eda489074215a7fdb663c
Fabio Marsiaj <fabiomarsiaj@gmail.com>
2021-03-28 00:50:15 -0300

    Configuring eureka server

--- eureka-server-ms/src/main/java/com/fabiomarsiaj/eurekaserverms/EurekaServerMsApplication.java
+++ eureka-server-ms/src/main/java/com/fabiomarsiaj/eurekaserverms/EurekaServerMsApplication.java
@@ -2,7 +2,9 @@ package com.fabiomarsiaj.eurekaserverms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerMsApplication {
 
